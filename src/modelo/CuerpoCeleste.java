479fd49b5b6b47fd2f76c8b7d8ab6d881d08fa93
Adriath <adrianarjonabravo@gmail.com>
2023-02-12 14:16:47 +0100

    Creación excepción ExcepcionCuerpoCeleste

---
+++ src/modelo/CuerpoCeleste.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Adrián Arjona
+ * @version 1.0 Febrero 2023
+ */
+public class CuerpoCeleste implements Serializable{
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 6: almacenando datos.
+    */
+    
+    private short codigoCuerpo ;
+    String nombre ;
+    String tipoObjeto ;
+    int diametro ;
+    
+}
---
+++ src/programacuerposcelestes/excepciones/ExcepcionCuerpoCeleste.java
@@ -0,0 +1,19 @@
+
+package programacuerposcelestes.excepciones;
+
+/**
+ *
+ * @author Adrián Arjona
+ * @version 1.0 Febrero 2023
+ */
+public class ExcepcionCuerpoCeleste extends Exception{
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 6: almacenando datos.
+    */
+    
+    public ExcepcionCuerpoCeleste (String msj){
+        
+        super(msj) ;
+    }
+}
