3450b178e621618631e95a8032c2fa0973305b65
Adriath <adrianarjonabravo@gmail.com>
2023-02-16 18:07:02 +0100

    Añadido el método para el menú en la clase alternativa al lanzador.

--- src/programacuerposcelestes/MetodosCuerpoCeleste.java
+++ src/programacuerposcelestes/MetodosCuerpoCeleste.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package programacuerposcelestes;
 
 /**
@@ -14,5 +11,18 @@ public class MetodosCuerpoCeleste {
     UNIDAD 6: almacenando datos.
     */
     
-    
+    public static void mostrarMenu(){ // MUESTRA MENÚ DE LA APLICACIÓN
+        
+        System.out.println("\n----------------------");
+        System.out.println("-------- MENÚ --------");
+        System.out.println("----------------------\n");
+        
+        System.out.println("1. Añadir cuerpo celeste");
+        System.out.println("2. Listar cuerpos celestes");
+        System.out.println("3. Buscar cuerpo celeste por código");
+        System.out.println("4. Buscar cuerpo celeste por tipo");
+        System.out.println("5. Borrar cuerpo celeste");
+        System.out.println("6. Borrar fichero de cuerpos celestes");
+        System.out.println("7. Salir de la aplicación");
+    }
 }
